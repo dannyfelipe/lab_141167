a907588c0059a4e563f8d006310927563828b656
Danny Felipe <dannyfmello@gmail.com>
2016-09-27 01:53:42 -0300

    #4

---
+++ lab141167/src/main/java/br/univel/ejb/ProcessVenda.java
@@ -0,0 +1,14 @@
+package br.univel.ejb;
+
+import javax.ejb.Stateless;
+
+import br.univel.classes.Venda;
+
+@Stateless
+public class ProcessVenda {
+
+	public void processarVenda(Venda venda) {
+
+    }
+
+}
