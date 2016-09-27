41158bb567eb3e0a37c746845f4263523bd34db5
Danny Felipe <dannyfmello@gmail.com>
2016-09-27 01:54:48 -0300

    #6

---
+++ lab141167/src/main/java/br/univel/mdb/MdbAuditoria.java
@@ -0,0 +1,67 @@
+package br.univel.mdb;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.logging.Logger;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import javax.jms.TextMessage;
+
+import org.hibernate.Session;
+
+import br.univel.classes.Log;
+import br.univel.classes.Venda;
+import br.univel.hibernate.HibernateConfig;
+
+@MessageDriven(name = "MdbAuditoria", activationConfig = {
+	    @ActivationConfigProperty(
+	    		propertyName = "destinationLookup",
+	    		propertyValue = "topic/TOPICVenda"),
+	    @ActivationConfigProperty(
+	    		propertyName = "destinationType",
+	    		propertyValue = "javax.jms.Topic"),
+	    @ActivationConfigProperty(
+	    		propertyName = "acknowledgeMode",
+	    		propertyValue = "Auto-acknowledge")
+	    })
+
+@TransactionManagement(TransactionManagementType.BEAN)
+public class MdbAuditoria implements MessageListener {
+
+	private final static Logger LOGGER = Logger.getLogger(MdbAuditoria.class.toString());
+
+	@Override
+	public void onMessage(Message arg0) {
+		if (arg0 instanceof ObjectMessage) {
+			ObjectMessage om = (ObjectMessage) arg0;
+			try {
+				Venda venda = (Venda) om.getObject();
+
+				Log logg = new Log();
+				logg.setData_time(new Timestamp(Calendar.getInstance().getTimeInMillis()));
+				;
+				logg.setNome_mdb("MdbAuditoria");
+				logg.setMsg(venda.toString());
+
+				Session session = HibernateConfig.getSessionFactory().openSession();
+				session.beginTransaction();
+				session.persist(logg);
+				session.getTransaction().commit();
+				session.close();
+
+			} catch (JMSException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+}
+
+}
