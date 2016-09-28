d63e9290ca12f24b9dfefe72f9d613e0df0916e6
Danny Felipe <dannyfmello@gmail.com>
2016-09-27 22:36:12 -0300

    fazendo parse do WSDL...
    [ERROR] http://localhost:8085/lab141167/EntregaWebService?WSDL é
    inacessível

---
+++ lab141167/lab141167-client/gerar.bat
@@ -0,0 +1,3 @@
+wsimport -p br.univel.classes.entrega -keep -d src_ws http://localhost:8085/lab141167/EntregaWebService?wsdl
+
+wsimport -p br.univel.classes.venda -keep -d src_ws http://localhost:8085/lab141167/VendaWebService?wsdl
---
+++ lab141167/lab141167-client/gerar2.bat
@@ -0,0 +1,3 @@
+wsimport -p br.univel.classes.Entrega -keep -d src_ws http://localhost:8085/lab141167/EntregaWebService?WSDL
+
+wsimport -p br.univel.classes.Venda -keep -d src_ws http://localhost:8085/lab141167/VendaWebService?WSDL
